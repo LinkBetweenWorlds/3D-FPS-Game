765dddf166634bf83513855dca471ca35153b6e1
William <32146967+LinkBetweenWorlds@users.noreply.github.com>
2021-02-16 16:17:18 -0800

    Add files via upload

---
+++ BlinkDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkDisplay : MonoBehaviour
+{
+    public bool BlinkReady;
+    public Text blinkText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlinkReady = false;
+        blinkText.text = "Blink : Not Ready";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBlink();
+        if (BlinkReady)
+        {
+            blinkText.text = "Blink : Ready";
+        }
+        else
+        {
+            blinkText.text = "Blink : Not Ready";
+        }
+    }
+    private void UpdateBlink()
+    {
+        GameObject Player = GameObject.Find("Main Camera");
+
+        TracerBlink Ready = Player.GetComponent<TracerBlink>();
+        BlinkReady = Ready.BlinkReady;
+    }
+}
---
+++ TracerBlink.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+public class TracerBlink : MonoBehaviour
+{
+    public float maxDistance = 4.0f;
+    public GameObject canvas;
+    public KeyCode Boost;
+    Vector3 destination;
+    Animator anim;
+    private float timer = 0;
+    private float BlinkTimer = 15;
+    public bool BlinkReady = false;
+    private void Start()
+    {
+        if (canvas != null)
+        {
+            anim = canvas.GetComponentInChildren<Animator>();
+            anim.enabled = false;
+        }
+        timer = 0;
+        BlinkTimer = 15;
+        BlinkReady = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= BlinkTimer && BlinkReady == false)
+        {
+            BlinkReady = true;
+        }
+        else 
+        {
+            timer += Time.deltaTime;
+        }
+        if (BlinkReady)
+        {
+            if (Input.GetKeyDown(Boost))
+            {
+                BlinkReady = false;
+                timer = 0;
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, maxDistance))
+                {
+                    destination = hit.point;
+                }
+                else
+                {
+                    destination = transform.
+                   position + transform.forward * maxDistance;
+                }
+            }
+            if (Input.GetKeyDown(Boost) && transform.position != destination)
+            {
+                destination.y += 0.5f;
+                transform.parent.position = destination;
+                if (anim != null)
+                    anim.enabled = true;
+            }
+            if (anim == null)
+            {
+                return;
+            }
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                anim.Rebind();
+                anim.enabled = false;
+            }
+        }
+    }
+}
